595be10e223821851b0917a1b0302f1877f122b5
Spyridakis Christos <spyridakischristos@gmail.com>
2025-01-28 12:10:21 +0200

    Debian apps install add lazygit & lazydocker

--- Debian/apps-install.sh
+++ Debian/apps-install.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# A simple script to easily install some of my go-to apps in a debian based system
+
+sudo apt update -y
+
 sudo apt install -y \
 	git vim nemo samba pdfarranger \
 	meld evince xournal gscan2pdf \
@@ -31,3 +37,12 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Docker 
 bash <(curl -sL https://gist.githubusercontent.com/CSpyridakis/0dd4e045dcddc68496c8403c098e0c19/raw/0fda1a194f559b05d6e26311c54090abe0cba4ca/install-docker.sh)
+
+# Lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+
+# Lazydocker
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
